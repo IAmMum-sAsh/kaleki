8135da0a9d3b6d48c3115f4f07f54daa659fadda
IAmMum-sAsh <vituchina-natasha11@yandex.ru>
2021-11-06 17:43:54 +0300

    добавление app-committer

---
+++ app-committer/src/main/java/ru/kaleki/commiter/CommiterApplication.java
@@ -0,0 +1,13 @@
+package ru.pominki.commiter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommiterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommiterApplication.class, args);
+	}
+
+}
