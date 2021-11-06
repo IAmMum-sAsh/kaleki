1e53a67750e5ceed4226fd80ea51fc1dc7d76411
IAmMum-sAsh <vituchina-natasha11@yandex.ru>
2021-11-06 17:46:10 +0300

    добавление app-presenter

---
+++ app-presenter/src/main/java/ru/kaleki/presenter/PresenterApplication.java
@@ -0,0 +1,13 @@
+package ru.pominki.presenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PresenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PresenterApplication.class, args);
+	}
+
+}
