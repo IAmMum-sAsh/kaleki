0a1a43a4597501787611460019e80dafc829134e
IAmMum-sAsh <vituchina-natasha11@yandex.ru>
2021-11-07 17:59:56 +0300

    рефактор

---
+++ kaleki-presenter/src/test/java/ru/mirea/kalekipresenter/KalekiPresenterApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mirea.kalekipresenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KalekiPresenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
