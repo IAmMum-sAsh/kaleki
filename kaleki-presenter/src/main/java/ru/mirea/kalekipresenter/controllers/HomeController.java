0b080978173a808321071d5665b7d00201c77452
IAmMum-sAsh <vituchina-natasha11@yandex.ru>
2021-11-08 15:30:27 +0300

    настройка Кафки presenter

---
+++ kaleki-presenter/src/main/java/ru/mirea/kalekipresenter/controllers/HomeController.java
@@ -0,0 +1,21 @@
+package ru.mirea.kalekipresenter.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import ru.mirea.kalekipresenter.models.Message;
+import ru.mirea.kalekipresenter.services.ProducerService;
+
+@RestController
+public class HomeController {
+
+    @Autowired
+    private ProducerService producerService;
+
+    @GetMapping("/generate")
+    public String generate(@RequestParam String message, @RequestParam Integer age) {
+        producerService.produce(new Message(message, age));
+        return "OK";
+    }
+}
